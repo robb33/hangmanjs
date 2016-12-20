6e357d87823dfce78fc7bef950109f4b1194dd0f
rob bee <rbenny@earthlink.net>
2016-12-20 08:25:19 -0500

    heroku files

--- assets/javascript/hmj.js
+++ assets/javascript/hmj.js
@@ -1,3 +1,29 @@
 // alert("hello world");
 
-$("#name").text('Hi!')
+// $("#name").text('Hi!')
+
+
+// function greetMe(yourName) {
+//   alert("Hello YOu " + yourName);
+// }
+
+// greetMe("World");
+
+
+
+
+$( "#target" ).keydown(function() {
+  alert( "Ok this how keydown works" );
+});
+
+
+
+$( "#other" ).click(function() {
+  $( "#target" ).keydown();
+});
+
+
+
+$( "#other" ).click(function() {
+  alert( "Ok this how keydown works" );
+});
